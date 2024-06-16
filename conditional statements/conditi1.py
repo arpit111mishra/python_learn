879ac299fd656f3ecc34754a124b427dfe1e7c8b
arpit111mishra <arpit111mishra@gmail.com>
2024-06-16 13:42:00 +0530

    conditional

---
+++ conditional statements/conditi1.py
@@ -0,0 +1,7 @@
+a= 56                        #if(): -elif():-else:
+if(a==56):
+    print("it's 56")
+elif(a==57):
+    print("it's 57")     #indentation
+else:
+    print("another number")         
