35e12eefa554e01f033a2d47f5d5b10ad711822a
arpit111mishra <arpit111mishra@gmail.com>
2024-06-16 22:47:33 +0530

    tuple done

---
+++ tuple/tuple1.py
@@ -0,0 +1,10 @@
+tup=(87,64,33,95,76) 
+print(tup)
+print(tup[0])
+#tup[0]=43   not mutable not allowed
+tup1=() #empty tuple
+print(tup1)
+tup2=(1,)    #single tuple value
+print(tup[:3])   #slicing 
+print(tup.index(87))
+print(tup.count(87))
