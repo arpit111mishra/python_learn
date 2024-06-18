d71eb580d1c491a503a1c1eec9a373db6fd75107
arpit111mishra <arpit111mishra@gmail.com>
2024-06-18 23:16:52 +0530

    loops

---
+++ loops/loops1.py
@@ -0,0 +1,16 @@
+count=1
+while count <= 5 :
+    print("arpit mishra")
+    count+=1
+list1=[1,4,9,16,25,36,49,64,81,100]
+i=0
+while i<len(list1):
+    print(list1[i])
+    i=i+1
+tupel=(1,4,9,16,25,36,49,64,81,100)
+j=0
+while j<=len(tupel):
+    if(tupel[j]==49):
+        print("found element :")
+        break
+    j+=1
