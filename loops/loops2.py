d62311e4820a01e6d1eef29434f141c7787e65a4
arpit111mishra <arpit111mishra@gmail.com>
2024-06-19 00:53:09 +0530

    loop completed

---
+++ loops/loops2.py
@@ -0,0 +1,19 @@
+list=[1,2,3]
+for i in list:
+    print(i)
+else:
+    print("end")
+strin="arpit mishra"
+for i in strin:
+    print(i)
+#range 
+for i in range(10):        #range(stop)
+    print(i)
+for j in range(30,40):      #range(start,end-1)
+    print(j)
+for k in range(40,80,2):    #range(start,end-1,step)
+    print(k)    
+for i in range(100,0,-1):
+    print(i)
+for i in range(10):
+    pass                    #pass vs continue    
